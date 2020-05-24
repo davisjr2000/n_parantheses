b7ded0b683fb11ab480b6913afa1a98b0c1308f6
Davis JR <davisjr2000@hotmail.com>
2020-05-23 20:21:26 -0500

    Finish challenge

--- n_parentheses.rb
+++ n_parentheses.rb
@@ -1,15 +1,14 @@
-def print_parentheses(array, step, number, open, closed, final_array = [], counter = 1)
-  counter += 1
+def print_parentheses(array, step, number, open, closed)
   if closed == number
     p array.join
   else
     if open > closed
       array[step] = ')'
-      print_parentheses(array, step + 1, number, open, closed + 1, final_array, counter)
+      print_parentheses(array, step + 1, number, open, closed + 1)
     end
     if number > open
       array[step] = '('
-      print_parentheses(array, step + 1, number, open + 1, closed, final_array, counter)
+      print_parentheses(array, step + 1, number, open + 1, closed)
     end
   end
 end
